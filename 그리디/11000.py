3908c6f5ca5a8dd9c028bfa1adf06b119e0387d5
김응관 <kwhn9486@naver.com>
2022-05-16 02:14:24 +0900

    solved

--- 그리디/11000.py
+++ 그리디/11000.py
@@ -1,22 +1,24 @@
+##시간복잡도를 최대한 줄여보자##
+
+
 import sys
 import heapq
+input = sys.stdin.readline
+
 num = int(input())
 lis = []
 last = []
 
 for i in range(num):
-    start, end = map(int, sys.stdin.readline().split())
+    start, end = map(int, input().split())
     lis.append([start, end])
 
-lis.sort(key=lambda x:x[1])
-count = 1
+lis.sort(key=lambda x:x[0])
 heapq.heappush(last, lis[0][1])
 
 for i in range(1, num):
     if lis[i][0] >= last[0]:
-        tmp = heapq.heappop(last)
-    else:
-        count += 1
+        heapq.heappop(last)
     heapq.heappush(last, lis[i][1])
 
-print(count)
+print(len(last))
