cee293126a38a4f31e1c76505fe54d8e4c10be77
김응관 <kwhn9486@naver.com>
2023-06-17 08:38:59 +0900

    solved

--- BFS/2606.py
+++ BFS/2606.py
@@ -1,30 +1,75 @@
-total = int(input())
-network = int(input())
+from collections import deque
 
-jip = [1]
-dic = {}
+computer = int(input())
+network = int(input())
+diction = {}
+path = [0] * computer
+diverse = [0] * computer
+
+
+def BFS(lis, count):
+    queue = deque([[lis]])
+
+    while queue:
+        tmp = queue.popleft()
+        container = []
+
+        for item in tmp:
+            for j in item[0]:
+                if j != 1 and path[j-1] == 0:
+                    path[j-1] = 1
+                    if j in diction:
+                        container.append((diction[j], j))
+                    count += 1
+                elif j != 1 and path[j-1] != 0:
+                    continue
+                elif j == 1:
+                    continue
+
+        if len(container) != 0:
+            queue.append(container)
+        else:
+            return count
+
+
+def reverse(num, count):
+    queue = deque([[num]])
+
+    while queue:
+        tmp = queue.popleft()
+        container = []
+
+        for item in tmp:
+            for key in diction.keys():
+                if item in diction[key] and path[key - 1] == 0:
+                    container.append(key)
+                    count += 1
+                elif item in diction[key] and path[key - 1] != 0:
+                    continue
+
+        if len(container) == 0:
+            return count
+        else:
+            queue.append(container)
+
 
 for i in range(network):
-    tmp = list(map(int, input().split()))
-    if tmp[0] in dic:
-        dic[tmp[0]] += [tmp[1]]
+    first, second = map(int, input().split())
+
+    if first not in diction:
+        diction[first] = [second]
     else:
-        dic[tmp[0]] = [tmp[1]]
+        diction[first].append(second)
 
-def BFS(index, dic):
-    if index in dic:
-        for i in range(len(dic[index])):
-            if dic[index][i] not in jip:
-                jip.append(dic[index][i])
-            BFS(dic[index][i], dic)
-    return len(jip) - 1
+    if second not in diction:
+        diction[second] = [first]
+    else:
+        diction[second].append(first)
 
-print(BFS(1, dic))
-
-
-
-
-
-
-
-
+if 1 in diction:
+    target = diction[1]
+    a = BFS((target, 1), 0)
+    b = reverse(1, 0)
+    print(a+b)
+else:
+    print(0)
